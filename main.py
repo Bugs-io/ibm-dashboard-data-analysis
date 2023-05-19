19ad6bdd381b8c985ae47bb79fce113727a4204b
Arturo Jacob <a01568525@tec.mx>
2023-05-19 14:20:56 -0600

    Organize code into separate modules and files

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import openpyxl
 from fastapi.exceptions import HTTPException
 import magic
 from fastapi import FastAPI, UploadFile, status, HTTPException
-import io
-import pandas as pd
-from pandas import DataFrame
-import openpyxl
 from fastapi.responses import StreamingResponse
+
+from utils import clean_dataset, convert_df_to_csv_string, read_excel_file
 
 app = FastAPI()
 
@@ -33,56 +30,12 @@ async def upload(file: UploadFile | None = None):
             detail=f"File type is not supported: {file_type}"
         )
     
-
-    wb = openpyxl.load_workbook(io.BytesIO(content))
-    ws = wb.active
-    df = DataFrame(ws.values)
-
-
-    for i, col in enumerate(df.iloc[0]):
-        df = df.rename(columns={i: col})
-
-    df = df.iloc[1:]
-
-    columns = ['uid', 'org', 'work_location', 'certification', 'issue_date', 'type']
-
-    if columns != list(df.columns):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="File not supported"
-        )
-    
-    if df.shape[0] <= 1:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Data should have at least one row of data"
-    )
-    
-    
-    df.loc[df.type == 'external certification', 'issue_date'] = df['certification'].str.split(
-    '(').str[::-1].str[0].str.split(')').str[0]
-    
-    df.loc[df.type == 'external certification', 'certification'] = df['certification'].str.rsplit('(', n=1).str[0].str.strip()
-
-    df.loc[df.type == 'external certification', 'issue_date'] = df['issue_date'].str.split(
-    ' ').str[2] + '-' + df['issue_date'].str.split(' ').str[1] + '-' + df['issue_date'].str.split(' ').str[0]
-
-
-    external_cert = df.loc[df.type == 'external certification']
-
-    external_cert.loc[:, 'issue_date'] = pd.to_datetime(external_cert['issue_date'], format='%Y-%b-%d').dt.date
-
-    df.loc[df.type == 'external certification', 'issue_date'] = external_cert['issue_date']
-
-
-    df['work_location'] = df['work_location'].fillna('Not specified')
-
-    stream = io.StringIO()
-    df.to_csv(stream, index=False)
-
+    df = read_excel_file(content)
+    df = clean_dataset(df)
+    csv_string = convert_df_to_csv_string(df)
 
     response = StreamingResponse(
-        iter([stream.getvalue()]), media_type="text/csv")
-    response.headers["Content-Disposition"] = "attachment; filename=export.csv"
-    
+        iter([csv_string]), media_type="text/csv")
+    response.headers["Content-Disposition"] = "attachment; filename=clean-data.csv"
+
     return response
